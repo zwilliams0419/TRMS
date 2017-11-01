6afb2787efa210f15748dd2f2b2a82ca189638fd
Zach Williams <zwilliams0419@gmail.com>
2017-11-01 01:28:37 -0400

    Added some incomplete logic to the LoginServlet

--- TRMS/src/main/java/com/revature/trms/servlet/LoginServlet.java
+++ TRMS/src/main/java/com/revature/trms/servlet/LoginServlet.java
@@ -1,10 +1,13 @@
 package com.revature.trms.servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.revature.trms.dao.DAOHolder;
 
 /**
  * Servlet implementation class LoginServlet
@@ -32,8 +35,20 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String uName = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		Integer userEmpId = null;
+		//userEmpId = DAOHolder.employeeDAO.loginCheck(uName, password);
+		if(userEmpId != null) {
+			request.getSession().setAttribute("userID", userEmpId);
+			//redirect to the servlet that generates the user's home page
+		}
+		else {
+			RequestDispatcher rd = request.getRequestDispatcher("/LOGINPAGENAME.html");
+			response.getWriter().write("Invalid login credentials");
+			rd.include(request, response);
+		}
 	}
 
 }
