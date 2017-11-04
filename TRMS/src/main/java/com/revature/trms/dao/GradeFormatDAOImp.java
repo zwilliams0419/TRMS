8af24546057df2d8fc4758a9bd642d1116f2f5c3
MikeHovell <Hovell.Michael@gmail.com>
2017-11-04 17:36:10 -0400

    Updating Grade Format DAO

--- TRMS/src/main/java/com/revature/trms/dao/GradeFormatDAOImp.java
+++ TRMS/src/main/java/com/revature/trms/dao/GradeFormatDAOImp.java
@@ -4,10 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.revature.trms.ConnectionFactory;
-import com.revature.trms.EventType;
 import com.revature.trms.GradeFormat;
 
 public class GradeFormatDAOImp implements GradeFormatDAO {
@@ -15,29 +15,50 @@ public class GradeFormatDAOImp implements GradeFormatDAO {
 	static Connection conn;
 	public GradeFormat getGradeFormat(int g_Id) {
 		
-		GradeFormat e = new GradeFormat();
+		GradeFormat g = new GradeFormat();
 		
 		try {
 			conn = ConnectionFactory.getConnection();
-			PreparedStatement stmt=conn.prepareStatement("SELECT * from GRADE_FORMAT WHERE EVENT_ID = ?");  
+			PreparedStatement stmt=conn.prepareStatement("SELECT * from GRADE_FORMAT WHERE GF_ID = ?");  
 			stmt.setInt(1, g_Id);
 			ResultSet rs=stmt.executeQuery(); 
 			while(rs.next())
 			{	
-				//e.setName(rs.getString("EVENT_NAME"));
-				//e.setRate(rs.getFloat("EVENT_COVERAGE"));
+				g.setName(rs.getString("GF_NAME"));
+				g.setMaxGrade(rs.getInt("GF_MAX_GRADE"));
+				g.setDefaultPassing(rs.getInt("GF_DEFAULT_PASSING"));
 			}
 
 		} 
 		catch (SQLException e1) {
 			e1.printStackTrace();
 		}
-		return e;	
+		return g;	
 	}
 
 	public List<GradeFormat> getAllGradeFormats() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		List<GradeFormat> myGradeFormats = new ArrayList<GradeFormat>();
+
+		GradeFormat g = new GradeFormat();
+
+		try {
+			conn = ConnectionFactory.getConnection();
+			PreparedStatement stmt=conn.prepareStatement("SELECT * from GRADE_FORMAT");  
+			ResultSet rs=stmt.executeQuery(); 
+			while(rs.next())
+			{	
+				g.setName(rs.getString("GF_NAME"));
+				g.setMaxGrade(rs.getInt("GF_MAX_GRADE"));
+				g.setDefaultPassing(rs.getInt("GF_DEFAULT_PASSING"));
+				myGradeFormats.add(g);
+			}
+
+		} 
+		catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+		return myGradeFormats;
 	}
 
 }
