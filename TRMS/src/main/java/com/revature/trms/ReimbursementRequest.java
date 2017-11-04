ae4692a1b1c67719f49403b994100b90e33bfc79
MikeHovell <Hovell.Michael@gmail.com>
2017-11-04 13:02:36 -0400

    Setting up ReimbursmentRequest

--- TRMS/src/main/java/com/revature/trms/ReimbursementRequest.java
+++ TRMS/src/main/java/com/revature/trms/ReimbursementRequest.java
@@ -1,10 +1,33 @@
 package com.revature.trms;
 
-public class ReimbursementRequest {
-	private int requestId, employeeId;
+import java.util.Date;
 
+public class ReimbursementRequest {
+	private int requestId;
+	private int employeeId;
+	private int bencoId;
+	private int gradeFormat;
+	private int eventType;
+	private Date eventDate;
+	private String address;
+	private String state;
+	private String city;
+	private int zip;
+	private String description;
+	private float cost;
+	private String justification;
+	private int approval;
+	private Date startDate;
+	private Date statusChangeDate;
+	
+	
+	
+	public ReimbursementRequest() {
+		super();
+	}
+	
 	public ReimbursementRequest(int i) {
-		
+		requestId = i;
 	}
 
 	public int getRequestId() {
@@ -21,5 +44,117 @@ public class ReimbursementRequest {
 
 	public void setEmployeeId(int employeeId) {
 		this.employeeId = employeeId;
+	}
+
+	public int getBencoId() {
+		return bencoId;
+	}
+
+	public void setBencoId(int bencoId) {
+		this.bencoId = bencoId;
+	}
+
+	public int getGradeFormat() {
+		return gradeFormat;
+	}
+
+	public void setGradeFormat(int gradeFormat) {
+		this.gradeFormat = gradeFormat;
+	}
+
+	public int getEventType() {
+		return eventType;
+	}
+
+	public void setEventType(int eventType) {
+		this.eventType = eventType;
+	}
+
+	public Date getEventDate() {
+		return eventDate;
+	}
+
+	public void setEventDate(Date eventDate) {
+		this.eventDate = eventDate;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public int getZip() {
+		return zip;
+	}
+
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public float getCost() {
+		return cost;
+	}
+
+	public void setCost(float cost) {
+		this.cost = cost;
+	}
+
+	public String getJustification() {
+		return justification;
+	}
+
+	public void setJustification(String justification) {
+		this.justification = justification;
+	}
+
+	public int getApproval() {
+		return approval;
+	}
+
+	public void setApproval(int approval) {
+		this.approval = approval;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getStatusChangeDate() {
+		return statusChangeDate;
+	}
+
+	public void setStatusChangeDate(Date statusChangeDate) {
+		this.statusChangeDate = statusChangeDate;
 	}
 }
